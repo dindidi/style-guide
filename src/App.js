49e79b060ef435cb027191e6256fadc643aea8c7
manoel jailton <manoeljailton79@gmail.com>
2022-08-13 11:29:54 -0300

    mod-32-style-guide

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
+
 import './App.css';
-import Dashboard from './components/Dashboard';
+import SignInSide from './components/SignInSide';
 
 function App() {
   return (
     <div className="App">
-      <Dashboard title="Meu Painel de Controle" />
+      <SignInSide title="Meu Painel de Controle" />
     </div>
   );
 }
